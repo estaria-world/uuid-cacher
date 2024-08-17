c19d8d29f33686e145d3b2f088f57cfb20945107
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-08-17 17:07:47 +0200

    add MojangUserProfile#getUUID method

--- src/main/kotlin/world/estaria/uuid/cacher/user/MojangUserProfile.kt
+++ src/main/kotlin/world/estaria/uuid/cacher/user/MojangUserProfile.kt
@@ -1,6 +1,8 @@
 package world.estaria.uuid.cacher.user
 
 import kotlinx.serialization.Serializable
+import world.estaria.uuid.cacher.extension.toUUID
+import java.util.*
 
 /**
  * @author Niklas Nieberler
@@ -10,4 +12,10 @@ import kotlinx.serialization.Serializable
 data class MojangUserProfile(
     val id: String,
     val name: String
-)
+) {
+
+    fun getUUID(): UUID {
+        return this.id.toUUID()
+    }
+
+}
